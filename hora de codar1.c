eb56f95e91f62a402314f167b976773e83fe5231
LucasBatalhaSecundino <202507321811@alunos.estacio.br>
2025-09-02 18:40:21 +0000

    desafionivelnovato

--- hora de codar1.c
+++ hora de codar1.c
@@ -1,61 +1,55 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-int main(){
-    int escolhajogador, escolhacomputador;
-    srand(time(0));
-    printf("Jogo de jokenpô!\n");
-    printf("escolha uma das três opções\n");
-    printf("1. Pedra\n");
-    printf("2. Papel\n");
-    printf("3. Tesoura\n");
-    printf("Você escolheu: \n");
-    scanf("%d", &escolhajogador);
+int main() {
+    int j, i;
+    char linha[10] = {'A','B','C','D','E','F','G','H','I','J'};
+    int coluna[10] = {1,2,3,4,5,6,7,8,9,10};
+
+    int tabuleiro[10][10] = {0}; // inicializa com zeros
+
+    //navio horizontal
+    int linhaNavio = 2; // linha C (2)
+    int colunaincio = 3; // coluna 4 (3)
+    int tamanhoNavio = 3; // tamanho do navio
+
+    for (i = 0; i < tamanhoNavio; i++)
+    {
+        tabuleiro[linhaNavio][colunaincio + i] = 3; // marca o navio no tabuleiro
+    }
+
+    // navio vertical
+    int colunaNavio = 7; 
+    int linhaInicio = 5; 
+    int tamanho = 3;
+    for (i = 0; i < tamanho; i++)
+    {
+        tabuleiro[linhaInicio + i][colunaNavio] = 3; // marca o navio no tabuleiro
+    }
     
-    escolhacomputador = rand() % 3 + 1;
-
-    switch (escolhajogador)
-    {
-    case 1:
-        printf("Jogador: Pedra - \n");
-
-        break;
-    case 2:
-    printf("Jogador: Papel - \n");
-    break;
-    case 3: 
-    printf("Jogador: Tesoura - \n");
-    break;
+    
 
 
-    default:
-    printf("Opção invalida!!");
-        break;
+
+
+
+    printf("   TABULEIRO NAVAL\n");
+
+    // Cabeçalho das colunas
+    printf("   ");
+    for (i = 0; i < 10; i++) {
+        printf("%2d ", coluna[i]);
     }
-    switch (escolhacomputador)
-    {
-    case 1:
-        printf("Computador: Pedra\n");
+    printf("\n");
 
-        break;
-    case 2:
-    printf("Computador: Papel\n");
-    break;
-    case 3: 
-    printf("Computador: Tesoura\n");
-        break;
-    }
-    if(escolhajogador == escolhacomputador){
-        printf("Empate!\n");}
-        else if ((escolhajogador == 1) && (escolhacomputador == 3) ||
-        (escolhajogador == 2) && (escolhacomputador == 1) ||
-        (escolhajogador == 3) && (escolhacomputador == 2))
-        {
-            printf("O jogador ganhou!!\n");
-        } else{
-            printf("Você perdeu!!\n");
+    // Linhas do tabuleiro
+    for (j = 0; j < 10; j++) {
+        printf("%c  ", linha[j]); // imprime a letra da linha
+        for (i = 0; i < 10; i++) {
+            printf("%2d ", tabuleiro[j][i]);
         }
-        
+        printf("\n");
     }
 
+
+    return 0;
+}
--- operadoreslogic.c
+++ operadoreslogic.c
@@ -1,32 +1,138 @@
 #include <stdio.h>
-
+ 
 int main() {
-    int idade;
-    float rendamensal;
-    int dependentes;
+    
+    char peca, direcao;
+    int i;
 
-    printf("Digite sua idade: \n");
-    scanf("%d", &idade);
-
-    printf("Digite sua renda mensal: \n");
-    scanf("%f", &rendamensal);
-
-    printf("Digite o número de dependentes: \n");
-    scanf("%d", &dependentes);
-
-    if (idade <= 18 || idade >= 60) {
-        if (rendamensal < 2000) {
-            if (dependentes > 2) {
-                printf("Você atende a todos os critérios.\n");
-            } else {
-                printf("Você não tem direito ao desconto devido ao número de dependentes.\n");
-            }
-        } else {
-            printf("Você não tem direito ao desconto devido à renda mensal ser superior a 2000 reais.\n");
+    printf("Escolha uma peca (T - Torre, B - Bispo, R - Rainha, C - Cavalo): ");
+    scanf(" %c", &peca);
+    if (peca == 'T') {
+        printf("Você escolheu a torre.\n");
+        for (i = 0; i <=5; i++)
+        {
+            printf("Direita\n");
         }
-    } else {
-        printf("Você não tem direito ao desconto devido à idade.\n");
+        
+    } else if (peca == 'B') {
+        printf("Você escolheu o bispo. Ele se move 8 casas em qualquer diagonal\n");
+        printf("Escolha uma direção (D - direita cima, E - esquerda cima, B - direita baixo, V - esquerda baixo: ");
+        scanf(" %c", &direcao);
+        if (direcao == 'D') {
+            printf("Você escolheu a diagonal direita cima.\n");
+        } else if (direcao == 'E') {
+            printf("Você escolheu a diagonal esquerda cima.\n");
+        } else if (direcao == 'B') {
+            printf("Você escolheu a diagonal direita baixo.\n");
+        } else if (direcao == 'V') {
+            printf("Você escolheu a diagonal esquerda baixo.\n");
+        } else {
+            printf("Direção inválida.\n");
+        }
+        i = 0;
+        do
+        {
+            if (direcao == 'D')
+            {
+                printf("Diagonal direita cima\n");
+            } else if (direcao == 'E') {
+                printf("Diagonal esquerda cima\n");
+            } else if (direcao == 'B') {
+                printf("Diagonal direita baixo\n");
+            } else if (direcao == 'V') {
+                printf("Diagonal esquerda baixo\n");
+            }
+        
+            i++;
+        } while (i <= 8);
+       
+
+    } else if (peca == 'R') {
+        printf("Você escolheu a rainha.\n");
+        i = 0;
+        while (i <= 5)
+        {
+            printf("Esquerda\n");
+            i++;
+        }
+        
+    } else if (peca == 'C')
+    {
+        printf("Você escolheu o cavalo.\n");
+        printf("O cavalo se move em L, pulando sobre as peças.\n");
+        printf("Movimento: duas casas para frente e uma para o lado.\n");
+        printf("Escolha uma direção(D = Cima direia, E = Cima esquerda, B = Baixo direita, V = Baixo esquerda): ");
+        scanf(" %c", &direcao);
+        if (direcao == 'D')
+        {
+           for (i = 0; i < 2; i++)
+           {
+            printf("Cima\n");
+
+            int j = 0;
+            while (j < 1)
+            {
+                printf("Direita\n");
+                j++;
+            }
+            
+           }
+        } else if (direcao == 'E') {
+            for (i = 0; i < 2; i++)
+           {
+            printf("Cima\n");
+
+            int j = 0;
+            while (j < 1)
+            {
+                printf("Esquerda\n");
+                j++;
+            }
+            
+           }
+        } else if (direcao == 'B') {
+            for (i = 0; i < 2; i++)
+           {
+            printf("Baixo\n");
+
+            int j = 0;
+            while (j < 1)
+            {
+                printf("Direita\n");
+                j++;
+            }
+            
+           }
+           } else if (direcao == 'V') {
+            for (i = 0; i < 2; i++)
+           {
+            printf("Baixo\n");
+
+            int j = 0;
+            while (j < 1)
+            {
+                printf("Esquerda\n");
+                j++;
+            }
+            
+           }
+           } else {
+            printf("Direção inválida.\n");
+           }
+        
+        
+        
     }
+     
+    else{
+        printf("Peça inválida.\n");
+    }
+
+    
+    
+    
+    
+
 
     return 0;
 }
